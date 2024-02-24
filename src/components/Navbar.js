6a0f12096ba4384d5a367b577b35b2a99a856935
reliable99 <abdulmuhminsodiq@gmail.com>
2024-02-24 21:41:25 +0100

    routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    const navLinkStyles = ({isActive}) => {
+        return{
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
+  return (
+    <nav>
+      <NavLink style={navLinkStyles} to="/">Home</NavLink>
+      <NavLink style={navLinkStyles} to="/about">About</NavLink>
+      <NavLink style={navLinkStyles} to="/contact">Contact</NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
